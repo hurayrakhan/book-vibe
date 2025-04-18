27399f8b997294ef834b3a692c2cceb282c64d10
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-18 23:40:07 +0600

    set all data dynamically to the book card and added icons

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { FaStar } from "react-icons/fa6";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
 
 
-const Books = () => {
+const Books = ({ book }) => {
+
+    const { publisher, bookName, author, image, yearOfPublishing, category, totalPages, rating } = book;
+    
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure className="px-10 pt-10">
+        <div className="card bg-base-100  shadow-xl border border-gray-300 p-5">
+            <figure className='p-5 bg-gray-200'>
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"
-                    className="rounded-xl" />
+                    src={image}
+                    alt="Shoes" className='h-40 w-30' />
             </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">{yearOfPublishing
+                    }</div>
+                </h2>
+                <div className='flex justify-between gap-4'>
+                    <div>
+                        <h3><span className='font-semibold'>Writer:</span> {author}</h3>
+                        <p><span className='font-semibold'>Category:</span> {category}</p>
+                        <p><span className='font-semibold'>Pages:</span> {totalPages}</p>
+                    </div>
+                    <div>
+                        <p><span className='font-semibold'>Publisher: </span>{publisher} </p>
+                        <div className='flex gap-3 items-center'>
+                            <div><p>{rating}</p></div>
+
+                            <div className='flex'>
+                                <FaStar />
+                                <FaRegStarHalfStroke />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import { useLoaderData } from 'react-router';
 import Books from '../Books/Books';
 
 const Home = () => {
-    
+
     const booksData = useLoaderData();
     
     return (
         <div>
             <Banner></Banner>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-10/12 mx-auto'>
             {
                 booksData.map(book => <Books key={book.bookId} book={book}></Books>)
             }
