2af73f63b9580ad49f0bc4d7394faa6a96448143
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-20 20:41:07 +0600

    footer link updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Footer = () => {
     return (
         <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
             <nav className="grid grid-flow-col gap-4">
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+
+                <NavLink className={({ isActive }) => isActive ? 'text-indigo-800  font-semibold ' : ''} to='/'>Home</NavLink>
+
+
+                <NavLink className={({ isActive }) => isActive ? 'text-indigo-800  font-semibold ' : ''} to='/about'>About</NavLink>
+
+
+                <NavLink className={({ isActive }) => isActive ? 'text-indigo-800  font-semibold ' : ''} to='/bookLists'>Book Lists</NavLink>
+
             </nav>
             <nav>
                 <div className="grid grid-flow-col gap-4">
